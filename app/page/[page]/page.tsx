ac2ca86880189bddc7bbc624abc6784f707a27fc
erickleite338 <erickleite338@gmail.com>
2024-12-13 19:20:19 +0000

    feat(page): Create dynamic page to display paginated posts
    - Configured `POSTS_PER_PAGE` with a limit of 5 posts per page
    - Implemented `generateStaticParams` to generate static parameters based
    on the total number of pages
    - Integrated the `useBlogPosts` hook to manipulate posts, initial
    display and pagination data
    - Used the `Main` component to render paginated posts

---
+++ app/page/[page]/page.tsx
@@ -0,0 +1,20 @@
+import { useBlogPosts } from 'hooks/use-blog-posts'
+import { allBlogs } from 'contentlayer/generated'
+import Main from 'app/Main'
+
+const POSTS_PER_PAGE = 5
+
+export const generateStaticParams = async () => {
+  const totalPages = Math.ceil(allBlogs.length / POSTS_PER_PAGE)
+  const paths = Array.from({ length: totalPages }, (_, i) => ({ page: (i + 1).toString() }))
+
+  return paths
+}
+
+export default async function Page(props: { params: Promise<{ page: string }> }) {
+  const params = await props.params
+  const pageNumber = parseInt(params.page as string)
+  const { posts, initialDisplayPosts, pagination } = useBlogPosts(pageNumber, POSTS_PER_PAGE)
+
+  return <Main posts={posts} initialDisplayPosts={initialDisplayPosts} pagination={pagination} />
+}
