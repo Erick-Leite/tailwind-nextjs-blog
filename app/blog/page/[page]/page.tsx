e2590e2efd9314b9c13d256f4a868ead792ba22b
erickleite338 <erickleite338@gmail.com>
2024-12-13 23:17:52 +0000

    feat(page): Create dynamic page to display paginated posts
    - Configured `POSTS_PER_PAGE` with a limit of 5 posts per page
    - Implemented `generateStaticParams` to generate static parameters based
    on the total number of pages
    - Integrated the `useBlogPosts` hook to handle posts, initial display,
    and pagination data
    - Used the `Main` component to render paginated posts

--- app/blog/page/[page]/page.tsx
+++ app/blog/page/[page]/page.tsx
@@ -1,6 +1,6 @@
-import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import { useBlogPosts } from 'hooks/use-blog-posts'
 import { allBlogs } from 'contentlayer/generated'
+import ListLayout from '@/layouts/ListLayoutWithTags'
 
 const POSTS_PER_PAGE = 5
 
@@ -13,16 +13,8 @@ export const generateStaticParams = async () => {
 
 export default async function Page(props: { params: Promise<{ page: string }> }) {
   const params = await props.params
-  const posts = allCoreContent(sortPosts(allBlogs))
   const pageNumber = parseInt(params.page as string)
-  const initialDisplayPosts = posts.slice(
-    POSTS_PER_PAGE * (pageNumber - 1),
-    POSTS_PER_PAGE * pageNumber
-  )
-  const pagination = {
-    currentPage: pageNumber,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-  }
+  const { posts, initialDisplayPosts, pagination } = useBlogPosts(pageNumber, POSTS_PER_PAGE)
 
   return (
     <ListLayout
