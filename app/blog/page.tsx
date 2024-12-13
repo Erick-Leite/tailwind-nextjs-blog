8c9b76a687fe41a55baf284acdc850c243c64e2f
erickleite338 <erickleite338@gmail.com>
2024-12-13 23:09:31 +0000

    feat(page): Refactor BlogPage to use custom hook for posts management
    - Replaced direct content handling with `useBlogPosts` custom hook for
    better encapsulation and reusability
    - Simplified logic for fetching `posts`, `initialDisplayPosts`, and
    `pagination`
    - Maintained the same metadata generation and layout structure

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,23 +1,14 @@
 import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
-import { allBlogs } from 'contentlayer/generated'
+import { useBlogPosts } from 'hooks/use-blog-posts'
 import { genPageMetadata } from 'app/seo'
 
+const PAGE_NUMBER = 1
 const POSTS_PER_PAGE = 5
 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
 export default function BlogPage() {
-  const posts = allCoreContent(sortPosts(allBlogs))
-  const pageNumber = 1
-  const initialDisplayPosts = posts.slice(
-    POSTS_PER_PAGE * (pageNumber - 1),
-    POSTS_PER_PAGE * pageNumber
-  )
-  const pagination = {
-    currentPage: pageNumber,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-  }
+  const { posts, initialDisplayPosts, pagination } = useBlogPosts(PAGE_NUMBER, POSTS_PER_PAGE)
 
   return (
     <ListLayout
